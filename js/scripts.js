1ee753327e26ff7ea362ff330e62cfdd4215c805
Cheshire Cat <thacheshirecat@gmail.com>
2018-06-01 10:25:35 -0700

    Add branching to display survey results based on one of the survey questions, and add additional CSS styling to the survey.

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
 
 $("#formSurvey").submit(function(event){
   event.preventDefault();
+  $("#rubyTrack").hide();
+  $("#cTrack").hide();
+  $("#cssTrack").hide();
 
   var userFirstName = $("input#first-name").val();
   var userLastName = $("input#last-name").val();
@@ -11,7 +14,13 @@ $("#formSurvey").submit(function(event){
   var fourthInput = parseInt($("input:radio[name=fourth-question]:checked").val());
   var fifthInput = parseInt($("input:radio[name=fifth-question]:checked").val());
 
-
+  if (firstInput === 1) {
+    $("#rubyTrack").show();
+  } else if (firstInput === 2) {
+    $("#cTrack").show();
+  } else {
+    $("#cssTrack").show();
+  }
 })
 
 
