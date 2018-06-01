b7a0f42d1cfed9146cbecee67b507f4e19f16800
Cheshire Cat <thacheshirecat@gmail.com>
2018-06-01 09:28:28 -0700

    Add variables to the forms submit function, and add 4 additional survay radios.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
 
+$("#formSurvey").submit(function(event){
+  event.preventDefault();
+
+  var userFirstName = $("input#first-name").val();
+  var userLastName = $("input#last-name").val();
+  var firstInput = parseInt($("input:radio[name=first-question]:checked").val());
+  var secondInput = parseInt($("input:radio[name=second-question]:checked").val());
+  var thirdInput = parseInt($("input:radio[name=third-question]:checked").val());
+  var fourthInput = parseInt($("input:radio[name=fourth-question]:checked").val());
+  var fifthInput = parseInt($("input:radio[name=fifth-question]:checked").val());
+
+
+})
 
 
 
